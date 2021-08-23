2507acdfe19ea3bff71ab8aae9259cade7b89f82
Yu Lian Ng <yulian.ng@thoughtworks.com>
2021-08-24 01:40:11 +0800

    add test cases for read

--- __test__/unit/crud/read.test.js
+++ __test__/unit/crud/read.test.js
@@ -5,13 +5,14 @@ const db = require('../../../db/index');
 jest.setTimeout(3000);
 jest.mock('../../../utils/logger.js');
 
+const pikachu = {
+  name: 'Pikachu',
+  japaneseName: 'ピカチュウ',
+  baseHP: 35,
+  category: 'Mouse Pokemon'
+};
 const pokemons = [
-  {
-    name: 'Pikachu',
-    japaneseName: 'ピカチュウ',
-    baseHP: 35,
-    category: 'Mouse Pokemon'
-  },
+  pikachu,
   {
     name: 'Squirtle',
     japaneseName: 'ゼニガメ',
@@ -49,11 +50,36 @@ describe('retrieve/read/find', () => {
     jest.clearAllMocks();
   });
 
-  /* eslint-disable no-unused-expressions */
+  /* eslint-disable no-unused-vars, no-unused-expressions, jest/no-disabled-tests */
+
   describe('findAllPokemons', () => {
     it('should return array with length 4', async () => {
       const retrieved = await findAllPokemons();
       expect(retrieved.length).toEqual(4);
+    });
+  });
+
+  describe.skip('findPokemons', () => {
+    describe('findPokemonsByName', () => {
+      it('should return a Pokemon', async () => {
+        const retrieved = await findPokemonByName('Pikachu');
+        expect(retrieved).toMatchObject(pikachu);
+      });
+
+      it('should throw error', async () => {
+        expect(async () => await findPokemonByName('Gibberish')).toThrow();
+      });
+    });
+
+    describe('findPokemonWithBaseHpMoreThan', () => {
+      it('should return array with baseHP more than', async () => {
+        const retrieved = await findPokemonsWithBaseHpMoreThan(40);
+
+        expect(retrieved.length).toEqual(2);
+
+        const retrievedNames = retrieved.map(pokemon => pokemon.name);
+        expect(retrievedNames).toMatchObject(['Squirtle', 'Wartortle']);
+      });
     });
   });
 });
